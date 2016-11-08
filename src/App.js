828d586eeb88cee0b95f1fa638df2f504084a398
Connor Sheehan <csheehan@mozilla.com>
2016-11-07 20:45:01 -0500

    Mess around and add material-ui

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import mac from './mac.jpeg';
+import RaisedButton from 'material-ui/RaisedButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+
+class MainMenuHeader extends Component {
+  render() {
+    var { header_name } = this.props;
+    return (
+      <div className="MainMenuHeader">
+        <h1>{header_name}</h1>
+      </div>
+    )
+  }
+}
+
+class MainMenuBuy extends Component {
+  render() {
+    return (
+      <div classname="MainMenuBuy">
+
+      </div>
+    )
+  }
+}
+
+class MainMenuOptions extends Component {
+  render() {
+    return (
+      <div className="MainMenuOptions">
+        <RaisedButton label="Test" />
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
+      <MuiThemeProvider>
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <MainMenuHeader header_name="Welcome to McMaster University"/>
+        <MainMenuBuy />
+        <MainMenuOptions />
       </div>
+      </MuiThemeProvider>
     );
   }
 }
+
+
 
 export default App;
